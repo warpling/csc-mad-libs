de6a3785a82b2776ab4cbfd66701e0317456395a
Ryan McLeod <ryanmcleod@gmail.com>
2013-11-18 00:21:50 -0800

    Cleaned up variables, added more verbs

--- inject.js
+++ inject.js
@@ -19,31 +19,30 @@
 
       var CSC_Classes = ["Discrete Structures I", "Introduction to Interactive Entertainment", "Introduction to Computer Organization", "Programming for Engineering Students", "Computer Programming for Scientists and Engineers", "C and Unix", "Professional Responsibilities", "Music Programming", "Special Problems", "Applied Parallel Computing", "Mobile Application Development", "Theory of Computation I", "Introduction to Computer Security", "User-Centered Interface Design and Development", "Computer Architecture", "Computer Security", "Thesis Seminar", "Thesis I", "Thesis II", "Thesis III", "Software Engineering", "Introduction to Database Systems", "Systems Programming", "Computer Vision", "Programming Languages I", "Capstone", "Operating Systems", "Networks"];
 
-      var Suicide_Methods = ["fart myself to death", "eat copious ammounts of fan dust", "eat nails instead", "seppuku", "autosacrifice myself to Bender", "guillotine myself with a MacBook Air", "eat batteries", "take a 500 watt power suply directly to the nipples", "bathe myself in radiation instead", "divide myself by zero", "use emacs as my editor", "stockpile soylent", "turn to grain alcohol for comfort", "withdraw from society and friends", "be living a sad, sedentary life in the CSL", "make web apps instead of studying"];
+      var verbs = ["fart myself to death", "eat copious ammounts of fan dust", "eat nails instead", "seppuku", "autosacrifice myself to Bender", "guillotine myself with a MacBook Air", "eat batteries", "take a 500 watt power suply directly to the nipples", "bathe myself in radiation instead", "divide myself by zero", "use emacs as my editor", "stockpile soylent", "drink grain alcohol for comfort", "withdraw from society and friends", "live a sad, sedentary life in the CSL", "make web apps instead of studying", "cry softly into my dalek pillow", "outsource my homework", "add a math minor", "start using caffinated soap"];
 
       function chooseOne(arr) {
         return arr[parseInt((Math.random() * arr.length))];
       }
 
       window.onload = function() {
-        var randomClass1, randomClass2, suicideMethod;
+        var randomClass1, randomClass2, verbage;
         
         // Choose your fate...
         randomClass1 = chooseOne(CSC_Classes);
         // To avoid overlap
         while (randomClass1 === (randomClass2 = chooseOne(CSC_Classes))){}
-        suicideMethod = chooseOne(Suicide_Methods);
+        verbage = chooseOne(verbs);
 
         // Apply the funnies...
         $('#class1').html(randomClass1);
         $('#class2').html(randomClass2);
         $('#feels').html(Math.random() > 0.5 ? "good" : "bad");
-        $('#methodOfDeading').html(suicideMethod);
+        $('#theFunnyGoesHere').html(verbage);
 
-        // We'll need this for the facebook sharer
+        // We'll need this for the facebook sharer         
         var finishedMadLib = $('#madlib').text();
         // INITIATE THE SHARING!
         $('.fbShare').attr("href", "http://www.facebook.com/sharer.php?s=100&p[title]=Hey guys, I'm thinking of taking...&p[url]=http://warpling.github.io/csc-mad-libs/&p[summary]=" + finishedMadLib);
       }
-
     })()
